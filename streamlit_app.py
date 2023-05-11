1e1b1ed576b5ea4033ab5a43dce054604be5b5d8
TKaushalK <133227952+TKaushalK@users.noreply.github.com>
2023-05-11 09:11:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast menu')
 
-streamlit.text('Apple')
-streamlit.text('juice')
+streamlit.text('🍞 Apple')
+streamlit.text('🥑 juice')
 streamlit.text('bread')
